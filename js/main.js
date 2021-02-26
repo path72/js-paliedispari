d25d35812407edd355b5b7a2f7e3e39a084ffb79
Salvatore Trimarchi <path72@gmail.com>
2021-02-26 18:03:38 +0100

    gioca a morra con skynet

--- js/main.js
+++ js/main.js
@@ -150,6 +150,101 @@ resumeBtn2.addEventListener('click',
   }
 );
 
+//###################################################### 
+// GIOCA A MORRA CON SKYNET
+
+// form sources
+var usrNumberMorraForm = document.getElementById('usr_nubmer_morra');
+var usrBetMorraForm    = document.getElementById('usr_bet_morra');
+
+// info display hooks
+var msgHtml3      = document.getElementById('msg_morra');
+var checkMsgHtml3 = document.getElementById('check_msg_morra');
+
+// button hooks
+var checkBtn3     = document.getElementById('check_btn_morra');
+var eraseBtn3     = document.getElementById('erase_btn_morra');
+var resumeBtn3    = document.getElementById('resume_btn_morra');
+
+// aux variables
+var betOKmorra = false;
+
+checkBtn3.addEventListener('click', 
+  function() {
+
+    // retrieving data form
+    var usrNumberValue = usrNumberMorraForm.value; // 1,2,3,4,5
+    var usrBetValue    = usrBetMorraForm.value;    // 1,2,3,4,5
+    var msg;
+
+    if (usrNumberValue == '' || usrBetValue == '') {
+      betOKmorra = false;
+      msg = 'Compila tutti i campi!';
+    } else {
+
+      betOKmorra = true;
+      // var usrBetIsEven = (usrBetValue == 'pari') ? true : false;
+      console.log('Utente numero: '+usrNumberValue+' - Scommessa: '+usrBetValue);
+
+      var skyNumber = random(0,5);
+      var skyBet = random(0,5);
+      console.log('SkyNet numero: '+skyNumber+' - Scommessa: '+skyBet);
+
+      var sum = sum2(usrNumberValue,skyNumber);
+      // var sumIsEven = isEven(sum); // array
+      console.log('La somma è '+sum);
+
+      var betMsg;
+      if      (sum == usrBetValue && sum != skyBet) betMsg = 'hai vinto!';
+      else if (sum != usrBetValue && sum == skyBet) betMsg = 'ha vinto SkyNet!';
+      else                                          betMsg = 'nulla di fatto!';
+      console.log(betMsg);
+      
+      var bestMsg = skyBest(random(1,8));
+      msg = 'Tu urli <strong class="highlight">'+usrBetValue+'</strong> '+
+            'e tiri <strong>'+usrNumberValue+'</strong><br>'+
+            'SkyNet urla <strong class="highlight">'+skyBet+'</strong>, '+
+            bestMsg+' e tira <strong>'+skyNumber+'</strong>,<br><br>'+
+            'La somma è <strong class="highlight">'+sum+'</strong>, <strong>'+betMsg+'</strong>';
+      // msg = 'Hai scelto il numero <strong>'+usrNumberValue+'</strong> '+
+      //       'scommettendo su <strong class="highlight">'+usrBetValue+'</strong><br><br>'+
+      //       'SkyNet ha scelto il numero <strong>'+skyNumber+'</strong><br><br>'+
+      //       'La somma <strong>'+sum+'</strong> è <strong class="highlight">'+sumIsEven[1]+'</strong>, '+
+      //       'allora <strong class="highlight">'+betMsg+'</strong>';
+    }
+
+    checkMsgHtml3.innerHTML = msg;
+    msgHtml3.className = 'show';
+
+  }
+);
+
+eraseBtn3.addEventListener('click', 
+  function() { 
+    usrNumberMorraForm.value  = '';
+    usrBetMorraForm.value     = '';
+  }
+);
+
+resumeBtn3.addEventListener('click', 
+  function() { 
+    msgHtml3.className      = 'hide';
+    checkMsgHtml3.innerHTML = '';
+    if (betOKmorra == true) {
+      usrNumberMorraForm.value = '';
+      usrBetMorraForm.value    = '';
+    }
+  }
+);
+
+//###################################################### 
+// FUNZIONI
+
+function skyBest(N) {
+  var bestList = ['bestemmia', 'impreca', 'sputa', 'si stizzisce', 'raglia', 'ti spintona', 'sbatte i pugni', 'scalcia'];
+  if (N>=1 && N<=8) return bestList[N-1];
+  else return bestList[0];
+}
 function random(n1,n2) {
   return Math.floor(Math.random()*(n2-n1+1))+n1;
 }
@@ -157,121 +252,12 @@ function sum2(n1,n2) {
   return parseInt(n1)+parseInt(n2);
 }
 function isEven(n) {
+  /*  restituisce array:
+        numero pari true/false
+        traduzione pari/dispari
+  */
   var ret = [];
   ret[0] = (n % 2 == 0) ? true : false;
   ret[1] = (ret[0]) ? 'pari' : 'dispari';
   return ret;
 }
-
-//###################################################### 
-// LA MORRA VERA
-
-// var usrBetNumberForm = document.getElementById('usr_bet_number');
-// var usrNumberForm    = document.getElementById('usr_nubmer');
-
-
-// //###################################################### 
-// //
-// // stampa primi 100 numeri
-// // multipli 3 > Fizz
-// // multipli 5 > Buzz
-// // multipli 3 e 5 > FizzBuzz
-
-// // form sources
-// var totalNumbersForm = document.getElementById('total_numbers');
-// var factor1Form      = document.getElementById('factor_1');
-// var factor2Form      = document.getElementById('factor_2');
-// var msg1Form         = document.getElementById('msg_1');
-// var msg2Form         = document.getElementById('msg_2');
-
-// // info display hooks
-// var msgHtml           = document.getElementById('msg');
-// var checkMsgHtml      = document.getElementById('check_msg');
-// var displayResultHtml = document.getElementById('display_result');
-// var resultListHtml    = document.getElementById('result_list');
-
-// // button hooks
-// var checkBtn     = document.getElementById('check_btn');
-// var eraseBtn     = document.getElementById('erase_btn');
-// var resumeBtn    = document.getElementById('resume_btn');
-
-// checkBtn.addEventListener('click', 
-//   function() { 
-
-//     // form data retrieving
-//     var N = totalNumbersForm.value,
-//         n1 = factor1Form.value,
-//         n2 = factor2Form.value,
-//         msg1 = msg1Form.value,
-//         msg2 = msg2Form.value;
-
-//     // consistency
-//     if (N=='' || n1=='' || n2=='' || msg=='' || msg2=='' || isNaN(N) || isNaN(n1)|| isNaN(n2)) {
-//       msgHtml.className = 'show';
-//       checkMsgHtml.innerHTML = 
-//           'Compila bene tutti i campi:<br>'+
-//           '<strong>Numeri totali</strong>,<br>'+
-//           '<strong>Divisore 1</strong> e <strong>Divisore 2</strong><br>'+
-//           'devono essere dei numeri!';
-//     } else {
-
-//       for (var i = 1; i<=N; i++) {
-  
-//         var j=i, msg, flag1, flag2;
-//         flag1 = (i % n1 == 0) ? true : false;
-//         flag2 = (i % n2 == 0) ? true : false;
-  
-//         if      ( flag1 && !flag2) msg = msg1;
-//         else if (!flag1 &&  flag2) msg = msg2;
-//         else if ( flag1 &&  flag2) msg = msg1 + msg2;
-//         else                       msg = i;
-  
-//         console.log(j+': '+msg);
-//         resultListHtml.innerHTML += '<tr><td>'+j+'</td><td>'+msg+'</td><tr>';
-  
-//         // view
-//         checkMsgHtml.innerHTML = 
-//               'Lista di <strong>'+N+' numeri</strong>:<br>'+
-//               'I multipli di <strong>'+n1+'</strong>: etichetta <strong>'+msg1+'</strong><br>'+
-//               'I multipli di <strong>'+n2+'</strong>: etichetta <strong>'+msg2+'</strong><br>'+
-//               'I multipli di <strong>'+n1+'</strong> e <strong>'+n2+'</strong>: etichetta <strong>'+msg1+msg2+'</strong><br>';
-//         msgHtml.className = 'show';
-//         displayResultHtml.className = 'show';
-
-//       }
-//     }
-//   }
-// );
-
-// eraseBtn.addEventListener('click', 
-//   function() { 
-    
-//     msgHtml.className = 'hide';
-//     checkMsgHtml.innerHTML = '';
-
-//     resultListHtml.innerHTML = '';
-//     displayResultHtml.className = 'hide';
-
-//     totalNumbersForm.value = '';
-//     factor1Form.value = '';
-//     factor2Form.value = '';
-//     msg1Form.value = '';
-//     msg2Form.value = '';
-
-//   }
-// );
-
-// resumeBtn.addEventListener('click', 
-//   function() { 
-    
-//     msgHtml.className = 'hide';
-//     checkMsgHtml.innerHTML = '';
-
-//     resultListHtml.innerHTML = '';
-//     displayResultHtml.className = 'hide';
-    
-//   }
-// );
-// //###################################################### 
-// //
-
