43689a7331e1e554ef4b0194850c97f76ee52b7c
Salvatore Trimarchi <path72@gmail.com>
2021-02-26 17:10:46 +0100

    la (finta) morra di skynet

--- js/main.js
+++ js/main.js
@@ -1,25 +1,17 @@
 //###################################################### 
-//
+// PALINDROMIA
 
 // form sources
-var usrStringForm      = document.getElementById('usr_srting');
-var usrPariDispariForm = document.getElementById('usr_paridispari');
-var usrNumberForm      = document.getElementById('usr_nubmer');
-// morra vera
-var usrBetNumberForm = document.getElementById('usr_bet_number');
-var usrNumberForm    = document.getElementById('usr_nubmer');
+var usrStringForm = document.getElementById('usr_srting');
 
 // info display hooks
-var msgHtml           = document.getElementById('msg');       // show/hide
-var checkMsgHtml      = document.getElementById('check_msg'); // content msg
+var msgHtml      = document.getElementById('msg');
+var checkMsgHtml = document.getElementById('check_msg');
 
 // button hooks
-var checkBtn     = document.getElementById('check_btn');
-var eraseBtn     = document.getElementById('erase_btn');
-var resumeBtn    = document.getElementById('resume_btn');
-
-//###################################################### 
-// PALINDROMO
+var checkBtn  = document.getElementById('check_btn');
+var eraseBtn  = document.getElementById('erase_btn');
+var resumeBtn = document.getElementById('resume_btn');
 
 checkBtn.addEventListener('click', 
   function() { 
@@ -27,6 +19,7 @@ checkBtn.addEventListener('click',
     // form data retrieving
     var string = usrStringForm.value;
     var result = isPalindrome(string);
+    var msg;
 
     if (string == '') {
       msg = 'Devi inserire una parola!'
@@ -44,25 +37,27 @@ checkBtn.addEventListener('click',
   }
 );
 
-resumeBtn.addEventListener('click', 
+eraseBtn.addEventListener('click', 
   function() { 
-    
-    msgHtml.className = 'hide';
-    checkMsgHtml.innerHTML = '';
     usrStringForm.value = '';
-    
   }
 );
 
-eraseBtn.addEventListener('click', 
+resumeBtn.addEventListener('click', 
   function() { 
-    
+    msgHtml.className = 'hide';
+    checkMsgHtml.innerHTML = '';
     usrStringForm.value = '';
-
   }
 );
 
 function isPalindrome(parola) {
+  /*  restituisce array:
+        palindromo true/false
+        lettere parola, senza spazi
+        lettere paraola al contrario, senza spazi 
+        lunghezza parola senza spazi
+  */ 
   var string = parola.replace(/ /g,'');
   var ret = [true,[],[],string.length];
   for (var i=0; i<ret[3]; i++) {
@@ -78,25 +73,82 @@ function isPalindrome(parola) {
 }
 
 //###################################################### 
-// 
+// LA (FINTA) MORRA DI SKYNET
 
-// usrPariDispari = 'pari' oppure 'dispari'
-var usrPariDispariForm = 'pari'; 
-var usrNumberForm      = 3;
+// form sources
+var usrNumberForm = document.getElementById('usr_nubmer');
+var usrBetForm    = document.getElementById('usr_bet');
 
-var usrBetIsEven = (usrPariDispariForm == 'pari') ? true : false;
-console.log('hai scommesso su pari: '+usrBetIsEven+'\ne scelto il numero: '+usrNumberForm);
+// info display hooks
+var msgHtml2      = document.getElementById('msg2');
+var checkMsgHtml2 = document.getElementById('check_msg2');
 
-var skyNumber = random(1,5);
-console.log('SkyNet ha scelto il numero: '+skyNumber);
+// button hooks
+var checkBtn2     = document.getElementById('check_btn2');
+var eraseBtn2     = document.getElementById('erase_btn2');
+var resumeBtn2    = document.getElementById('resume_btn2');
 
-var sum = sum2(usrNumberForm,skyNumber);
-var sumIsEven = isEven(sum);
-console.log('la somma è '+sum+', pari: '+sumIsEven);
+// aux variables
+var betOK = false;
 
-var msg = 'ha vinto SkyNet!';
-if ((sumIsEven && usrBetIsEven) || (!sumIsEven && !usrBetIsEven)) msg = 'hai vinto!';
-console.log(msg);
+checkBtn2.addEventListener('click', 
+  function() {
+
+    // retrieving data form
+    var usrNumberValue = usrNumberForm.value; // 1,2,3,4,5
+    var usrBetValue    = usrBetForm.value;    // pari,dispari
+    var msg;
+
+    if (usrNumberValue == '' || usrBetValue == '') {
+      betOK = false;
+      msg = 'Compila tutti i campi!';
+    } else {
+
+      betOK = true;
+      var usrBetIsEven = (usrBetValue == 'pari') ? true : false;
+      console.log('Numero: '+usrNumberValue+' - Scommessa: '+usrBetValue);
+
+      var skyNumber = random(1,5);
+      console.log('SkyNet, numero: '+skyNumber);
+
+      var sum = sum2(usrNumberValue,skyNumber);
+      var sumIsEven = isEven(sum); // array
+      console.log('La somma '+sum+' è '+sumIsEven[1]);
+
+      var betMsg = 'ha vinto SkyNet!';
+      if ((sumIsEven[0] && usrBetIsEven) || (!sumIsEven[0] && !usrBetIsEven)) betMsg = 'hai vinto!';
+      console.log(betMsg);
+
+      msg = 'Hai scelto il numero <strong>'+usrNumberValue+'</strong> '+
+            'scommettendo su <strong class="highlight">'+usrBetValue+'</strong><br><br>'+
+            'SkyNet ha scelto il numero <strong>'+skyNumber+'</strong><br><br>'+
+            'La somma <strong>'+sum+'</strong> è <strong class="highlight">'+sumIsEven[1]+'</strong>, '+
+            'allora <strong class="highlight">'+betMsg+'</strong>';
+    }
+
+    checkMsgHtml2.innerHTML = msg;
+    msgHtml2.className = 'show';
+
+  }
+);
+
+eraseBtn2.addEventListener('click', 
+  function() { 
+    usrNumberForm.value  = '';
+    usrBetForm.value     = '';
+  }
+);
+
+resumeBtn2.addEventListener('click', 
+  function() { 
+    msgHtml2.className      = 'hide';
+    checkMsgHtml2.innerHTML = '';
+    if (betOK == true) {
+      usrNumberForm.value     = '';
+      usrBetForm.value        = '';
+    }
+  }
+);
 
 function random(n1,n2) {
   return Math.floor(Math.random()*(n2-n1+1))+n1;
@@ -105,13 +157,17 @@ function sum2(n1,n2) {
   return parseInt(n1)+parseInt(n2);
 }
 function isEven(n) {
-  return (n % 2 == 0) ? true : false;
+  var ret = [];
+  ret[0] = (n % 2 == 0) ? true : false;
+  ret[1] = (ret[0]) ? 'pari' : 'dispari';
+  return ret;
 }
 
+//###################################################### 
+// LA MORRA VERA
 
-
-
-
+// var usrBetNumberForm = document.getElementById('usr_bet_number');
+// var usrNumberForm    = document.getElementById('usr_nubmer');
 
 
 // //###################################################### 
